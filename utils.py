72e28e7d3cabae347711fb209b2be184edd07256
Alex1966design <a.kamenetsky@mail.ru>
2025-04-04 18:44:48 +0300

    Add files via upload

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
-import os
-import streamlit as st
+# utils.py
 
+import os
 from langchain_community.document_loaders import TextLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
+import streamlit as st
 
 # Пути к данным и базе
 DATA_PATH = "data/zdorovoe_info.md"
@@ -27,7 +28,6 @@ def load_vectorstore():
 
         embedding = OpenAIEmbeddings(api_key=st.secrets["OPENAI_API_KEY"])
         vectordb = Chroma.from_documents(docs, embedding=embedding, persist_directory=DB_DIR)
-
     else:
         print("Загружаю существующую векторную базу...")
         embedding = OpenAIEmbeddings(api_key=st.secrets["OPENAI_API_KEY"])
@@ -35,10 +35,10 @@ def load_vectorstore():
 
     return vectordb.as_retriever()
 
-# Получение ответа на вопрос
+# Получение ответа
 def get_answer(question, retriever):
     qa_chain = RetrievalQA.from_chain_type(
-        llm=ChatOpenAI(model="gpt-4o", temperature=0.5, api_key=st.secrets["OPENAI_API_KEY"]),
+        llm=ChatOpenAI(model="gpt-4o", temperature=0.5),
         retriever=retriever,
         return_source_documents=False
     )
